753e7b9945affd4e517acd2d6ffe40578a66381c
root <root@ip-172-31-45-226.ec2.internal>
2019-09-07 13:24:18 +0000

    Adil updates Microservcies

--- connection/database.js
+++ connection/database.js
@@ -3,13 +3,13 @@ f = require('util').format,
 assert = require('assert')
 function connectionDatabase() {
     return new Promise((resolve, reject) => {
-        var url  = 'mongodb://binance:binance2019readwrite@localhost:27017/?authMechanism=SCRAM-SHA-1&authSource=binance';
+        var url  = 'mongodb://root:95bcqr1vizz@digiebot-shard-00-00-bhelp.mongodb.net:27017,digiebot-shard-00-01-bhelp.mongodb.net:27017,digiebot-shard-00-02-bhelp.mongodb.net:27017/test?ssl=true&replicaSet=Digiebot-shard-0&authSource=admin&retryWrites=true&w=majority';
 
             MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
                 assert.equal(null, err);
                 if (err){
                     reject(err);
-                }else{  
+                }else{
                 const db = client.db('binance');
                 resolve(db)
                 }//End of  connection success
