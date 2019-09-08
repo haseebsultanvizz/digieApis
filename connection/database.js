2d909b884433ab3afb028f428acd85107001701e
root <root@guest.digiebot.com>
2019-09-08 06:42:02 -0400

    mongo connection updagted

--- connection/database.js
+++ connection/database.js
@@ -3,10 +3,10 @@ f = require('util').format,
 assert = require('assert')
 function connectionDatabase() {
     return new Promise((resolve, reject) => {
-        var url  = 'mongodb://binance:binance2019readwrite@localhost:27017/?authMechanism=SCRAM-SHA-1&authSource=binance';
+        //var url  = 'mongodb://binance:binance2019readwrite@localhost:27017/?authMechanism=SCRAM-SHA-1&authSource=binance';
         //var url = "mongodb://root:95bcqr1vizz@digiebot-shard-00-00-bhelp.mongodb.net:27017,digiebot-shard-00-01-bhelp.mongodb.net:27017,digiebot-shard-00-02-bhelp.mongodb.net:27017/test?ssl=true&replicaSet=Digiebot-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-        //var url = "mongodb://root:95bcqr1Vizz@digiebot-shard-00-00-bhelp.mongodb.net:27017,digiebot-shard-00-01-bhelp.mongodb.net:27017,digiebot-shard-00-02-bhelp.mongodb.net:27017/test?ssl=true&replicaSet=Digiebot-shard-0&authSource=admin&retryWrites=true&w=majority";
+        var url = "mongodb://root:95bcqr1Vizz@digiebot-shard-00-00-bhelp.mongodb.net:27017,digiebot-shard-00-01-bhelp.mongodb.net:27017,digiebot-shard-00-02-bhelp.mongodb.net:27017/test?ssl=true&replicaSet=Digiebot-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 
             MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
