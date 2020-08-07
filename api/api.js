731ad3a65bb8c0f552ab9ce7e09e83f9b9f7636a
Umer Abbas <umer_abbass.vizz@vizzwebsolutions.com>
2020-08-07 11:45:13 +0500

    show log

--- api/api.js
+++ api/api.js
@@ -13886,7 +13886,7 @@ router.post('/updateDailyTradeSettings', async (req, res) => {
     var user_id = req.body.user_id
     let exchange = req.body.exchange
     let application_mode = typeof req.body.application_mode != 'undefined' && req.body.application_mode != '' ? req.body.application_mode : 'live'
-    if (typeof exchange != 'undefined' && exchange != '' && typeof user_id != 'undefined' && (user_id != '' || user_id != null)) {
+    if (typeof exchange != 'undefined' && exchange != '' && typeof user_id != 'undefined' && (user_id != '' || user_id != null || user_id != 'null')) {
         //get all users with auto trade settings
         conn.then(async(db) => {
             let collectionName = exchange == 'binance' ? 'auto_trade_settings' : 'auto_trade_settings_' + exchange
