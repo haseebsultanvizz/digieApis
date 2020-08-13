cc59e13709d10dbfec92b78dba2b044536843e76
Umer Abbas <umer_abbass.vizz@vizzwebsolutions.com>
2020-08-13 15:39:19 +0500

    orders stats API

--- api/api.js
+++ api/api.js
@@ -2491,6 +2491,9 @@ router.post('/listOrderListing', async (req, resp) => {
     countArr['totalBuyCount'] = all2Count;
     countArr['totalSoldCount'] = all1Count;
     //get user balance for listing on list-order page
+
+    // countArr = await getOrderStats(postDAta)
+
     var userBalanceArr = []
     userBalanceArr = await get_user_wallet(admin_id, exchange)
 
@@ -14492,5 +14495,475 @@ router.get('/fixUsdWorth', async (req, res) => {
     
 
 })
+
+async function getOrderStats(postData){
+
+        // application_mode = "live"
+        // admin_id = "5c0912b7fc9aadaac61dd072"
+        // exchange = "binance"
+
+        postData['skip'] = 0
+        postData['limit'] = 20
+        postData['coins'] = []
+        postData['order_type'] = ""
+        postData['trigger_type'] = ""
+        postData['order_level'] = ""
+        postData['start_date'] = ""
+        postData['end_date'] = ""
+        postData['status'] = "open"
+
+        var admin_id = postData.admin_id;
+        var application_mode = postData.application_mode;
+        var postDAta = postData;
+        var search = {};
+        //if filter values exist for order list create filter on the base of selected filters
+        if (postDAta.coins != '') {
+            //search on the bases of coins
+            search['symbol'] = {
+                '$in': postDAta.coins
+            }
+        }
+
+        if (postDAta.order_type != '') {
+            //search on the base of order type mean manual order or trigger order
+            search['order_type'] = postDAta.order_type
+        }
+
+        if (postDAta.trigger_type != '') {
+            //seatch on the base of specific trigger
+            search['trigger_type'] = postDAta.trigger_type
+        }
+
+        if (postDAta.order_level != '') {
+            //search on the base of order level for auto trading
+            search['order_level'] = postDAta.order_level
+        }
+
+        var count = 0;
+        var i;
+        for (i in search) {
+            if (search.hasOwnProperty(i)) {
+                count++;
+            }
+        }
+
+        var exchange = postDAta.exchange;
+        var collectionName = (exchange == 'binance') ? 'buy_orders' : 'buy_orders_' + exchange;
+        //::::::::::::::::::::::::::::::::::::::::::::::::
+        //Filter_1 part for count number of parent orders
+        var filter_1 = {};
+        filter_1['parent_status'] = 'parent';
+        filter_1['admin_id'] = admin_id;
+        filter_1['application_mode'] = application_mode;
+        filter_1['status'] = {
+            '$in': ['new', 'takingOrder']
+        }
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_1['created_date'] = obj;
+        }
+
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_1[key] = value;
+            }
+        }
+        //:::::::::::::::::::::: End of count parent ordes Filter :::::::::::::: 
+        //count parent orders Promise
+        var parentCountPromise = countCollection(collectionName, filter_1);
+
+        //::::::::::::: filter_2 count new orders for order listing ::::::::::::
+        var filter_2 = {};
+        filter_2['status'] = {
+            '$in': ['new', 'new_ERROR', 'BUY_ID_ERROR']
+        };
+        filter_2['price'] = {
+            '$nin': ['', null]
+        };
+        filter_2['admin_id'] = admin_id;
+        filter_2['application_mode'] = application_mode;
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_2['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_2[key] = value;
+            }
+        }
+        //:::::::::: End of filter_2 for couting new orders :::::::::::::
+        //Promise for count new orders
+        var newCountPromise = countCollection(collectionName, filter_2);
+
+        //:::::::::::::::: filter_3 for count open order :::::::::::::::::
+        var filter_3 = {};
+        filter_3['status'] = {
+            '$in': ['FILLED', 'FILLED_ERROR', 'SELL_ID_ERROR']
+        }
+        filter_3['is_sell_order'] = 'yes';
+        filter_3['is_lth_order'] = {
+            $ne: 'yes'
+        };
+        filter_3['admin_id'] = admin_id;
+        filter_3['application_mode'] = application_mode;
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_3['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_3[key] = value;
+            }
+        }
+        //::::::::::::::::End of filter_3 for count open order :::::::::::::::::
+
+        //::::::::: Open-orders count Promise :::::::::::::::::::::::::::::::::
+        var openCountPromise = countCollection(collectionName, filter_3);
+
+        //::::::::::::::: filter_33 for count filled orders :::::::::::::
+        var filter_33 = {};
+        filter_33['status'] = {
+            '$in': ['FILLED', 'fraction_submitted_buy', 'FILLED_ERROR']
+        }
+        filter_33['admin_id'] = admin_id;
+        filter_33['application_mode'] = application_mode;
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_33['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_33[key] = value;
+            }
+        }
+        //:::::::::::::::End of  filter_33 for count filled orders :::::::::::::
+        //promise of count filled orders
+        var filledCountPromise = countCollection(collectionName, filter_33);
+
+        //:::::::::::; filter_4 for count all canceled orders ::::::::::::;
+        var filter_4 = {};
+        filter_4['status'] = 'canceled';
+        filter_4['admin_id'] = admin_id;
+        filter_4['application_mode'] = application_mode;
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_4['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_4[key] = value;
+            }
+        }
+        //:::::::::::: End of  filter_4 for count all canceled orders ::::::::::::;
+        //Promise for canceled count orders :::::::::::::::::
+        var cancelCountPromise = countCollection(collectionName, filter_4);
+
+        //:::::::::::; filter_errors for count all Errors orders ::::::::::::;
+        var filter_errors = {};
+        filter_errors['status'] = { '$nin': ['new', 'FILLED', 'fraction_submitted_buy', 'canceled', 'LTH', 'submitted', 'submitted_for_sell', 'fraction_submitted_sell'] }
+        filter_errors['parent_status'] = { '$exists': false };
+        filter_errors['admin_id'] = admin_id;
+        filter_errors['application_mode'] = application_mode;
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_errors['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_errors[key] = value;
+            }
+        }
+        //:::::::::::: End of  filter_errors for count all Errors orders ::::::::::::;
+        //Promise for Errors count orders :::::::::::::::::
+        var errorsCountPromise = countCollection(collectionName, filter_errors);
+
+        //::::::: filter_5  for count all error orders ::::::::::::::::::::::: 
+        var filter_5 = {};
+        filter_5['status'] = 'error';
+        filter_5['admin_id'] = admin_id;
+        filter_5['application_mode'] = application_mode;
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_5['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_5[key] = value;
+            }
+        }
+        //::::::: End of  filter_5  for count all error orders ::::::::::::::::::::::: 
+        //Promise for count error orders ::::::::::
+        var errorCountPromise = countCollection(collectionName, filter_5);
+
+        //::::::::::::: filter_6 for count all lth order :::::::::::::::::::
+        var filter_6 = {};
+        filter_6['status'] = {
+            $in: ['LTH', 'LTH_ERROR']
+        };
+        filter_6['admin_id'] = admin_id;
+        filter_6['application_mode'] = application_mode;
+        filter_6['is_sell_order'] = 'yes';
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_6['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_6[key] = value;
+            }
+        }
+        //::::::::::::: End of filter_6 for count all lth order :::::::::::::::::::
+        //Promise for count lth orders
+        var lthCountPromise = countCollection(collectionName, filter_6);
+
+        //:::::::::::::  filter_7 for count all submitted order :::::::::::::::::::
+        var filter_7 = {};
+        filter_7['status'] = {
+            '$in': ['submitted', 'submitted_for_sell', 'fraction_submitted_sell', 'submitted_ERROR']
+        }
+        filter_7['admin_id'] = admin_id;
+        filter_7['application_mode'] = application_mode;
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_7['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_7[key] = value;
+            }
+        }
+        //::::::::::::: End of filter_7 for count all submitted order :::::::::::::::::::
+        //promise for count submitted orders
+        var submittedCountPromise = countCollection(collectionName, filter_7);
+
+        var collectionName = (exchange == 'binance') ? 'sold_buy_orders' : 'sold_buy_orders_' + exchange;
+
+        //::::::::::::: filter_8 for count all sold order :::::::::::::::::::
+        var filter_8 = {};
+        filter_8['admin_id'] = admin_id;
+        filter_8['application_mode'] = application_mode;
+        filter_8['$or'] = [
+            { 'resume_status': 'completed', 'trading_status': 'complete' },
+            { 'is_sell_order': 'sold', 'resume_order_id': { '$exists': false } }
+        ];
+        if (!digie_admin_ids.includes(admin_id)) {
+            filter_8['$or'][0]['show_order'] = 'yes'
+        }
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_8['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_8[key] = value;
+            }
+        }
+        //::::::::::::: End of filter_8 for count all sold order :::::::::::::::::::
+        //Promise for count all sold orders 
+        var soldCountPromise = countCollection(collectionName, filter_8);
+
+        //::::::::::::: filter_9 for count all lth_pause order :::::::::::::::::::
+        var filter_9 = {};
+        filter_9['admin_id'] = admin_id;
+        filter_9['application_mode'] = application_mode;
+        filter_9['status'] = { '$in': ['FILLED', 'pause'] };
+        filter_9['is_sell_order'] = {
+            '$in': ['pause', 'resume_pause']
+            // '$in': ['pause', 'resume_pause', 'resume_complete']
+        };
+        filter_9['resume_status'] = { '$ne': 'completed' }
+        filter_9['show_order'] = { '$ne': 'no' };
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_9['created_date'] = obj;
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_9[key] = value;
+            }
+        }
+        //::::::::::::: End of filter_9 for count all lth_pause  order :::::::::::::::::::
+        //Promise for count all lth_pause orders
+        var lthPauseCountPromise = countCollection(collectionName, filter_9);
+
+        //Count all tab
+        let filter_all = {};
+        filter_all['application_mode'] = postDAta.application_mode
+        filter_all['admin_id'] = postDAta.admin_id
+
+        if (postDAta.start_date != '' || postDAta.end_date != '') {
+            let obj = {}
+            if (postDAta.start_date != '') {
+                obj['$gte'] = new Date(postDAta.start_date);
+            }
+            if (postDAta.end_date != '') {
+                obj['$lte'] = new Date(postDAta.end_date);
+            }
+            filter_all['created_date'] = obj
+        }
+        if (!digie_admin_ids.includes(postDAta.admin_id)) {
+            filter_all['is_sell_order'] = {
+                '$nin': ['pause', 'resume_pause']
+                // '$in': ['pause', 'resume_pause', 'resume_complete']
+            };
+            filter_all['resume_status'] = { '$ne': 'complete' }
+            filter_all['resume_order_id'] = { '$exists': false };
+            filter_all['resumed_parent_buy_order_id'] = { '$exists': false }
+        }
+        if (count > 0) {
+            for (let [key, value] of Object.entries(search)) {
+                filter_all[key] = value;
+            }
+        }
+        let soldOrdercollection = (exchange == 'binance') ? 'sold_buy_orders' : 'sold_buy_orders_' + exchange;
+        let all1Promise = countCollection(soldOrdercollection, filter_all);
+        // filter_all['parent_status'] = {'$exists': false}
+        let buyOrdercollection = (exchange == 'binance') ? 'buy_orders' : 'buy_orders_' + exchange;
+        let all2Promise = countCollection(buyOrdercollection, filter_all);
+        //End count All tab
+
+        //Resolve promised for count order for all tabs
+        var PromiseResponse = await Promise.all([parentCountPromise, newCountPromise, openCountPromise, cancelCountPromise, errorCountPromise, lthCountPromise, submittedCountPromise, soldCountPromise, filledCountPromise, lthPauseCountPromise, all1Promise, all2Promise, errorsCountPromise]);
+
+        var parentCount = PromiseResponse[0];
+        var newCount = PromiseResponse[1];
+        var openCount = PromiseResponse[2];
+        var cancelCount = PromiseResponse[3];
+        var errorCount = PromiseResponse[4];
+        var lthCount = PromiseResponse[5];
+        var submitCount = PromiseResponse[6];
+        var soldCount = PromiseResponse[7];
+        var filledCount = PromiseResponse[8];
+        var lthPauseCount = PromiseResponse[9];
+        var all1Count = PromiseResponse[10];
+        var all2Count = PromiseResponse[11];
+        var errorsCount = PromiseResponse[12];
+
+        // var totalCount = parseFloat(parentCount) + parseFloat(newCount) + parseFloat(openCount) + parseFloat(cancelCount) + parseFloat(errorCount) + parseFloat(lthCount) + parseFloat(submitCount) + parseFloat(soldCount) + parseFloat(lthPauseCount);
+
+        var totalCount = parseFloat(all1Count) + parseFloat(all2Count);
+
+        var countArr = {};
+        countArr['totalCount'] = totalCount;
+        countArr['parentCount'] = parentCount;
+        countArr['newCount'] = newCount;
+        countArr['openCount'] = openCount;
+        countArr['canceledCount'] = cancelCount;
+        countArr['errorsCount'] = errorsCount;
+        countArr['errorCount'] = errorCount;
+        countArr['lthCount'] = lthCount;
+        countArr['submitCount'] = submitCount;
+        countArr['soldCount'] = soldCount;
+        countArr['filledCount'] = filledCount;
+        countArr['lthPauseCount'] = lthPauseCount;
+        countArr['totalBuyCount'] = all2Count;
+        countArr['totalSoldCount'] = all1Count;
+
+        return countArr
+}
+
+//getOrderStats
+router.post('/getOrderStats', async (req, res) => {
+    let admin_id = req.body.user_id
+    let exchange = req.body.exchange
+    let application_mode = req.body.application_mode
+    
+    if (typeof exchange != 'undefined' && typeof exchange != 'undefined' && typeof admin_id != 'undefined' && typeof admin_id != 'undefined' && typeof application_mode != 'undefined' && typeof application_mode != 'undefined') {
+
+        let postData = {
+            'admin_id': admin_id,
+            'exchange': exchange,
+            'application_mode': application_mode,
+        }
+
+        let stats = await getOrderStats(postData)
+        res.send({
+            status: true,
+            data: stats,
+            message: 'Orders stats found successfully',
+        })
+    } else {
+        res.send({
+            status: false,
+            message: 'exchange and user_id, and application_mode are required',
+        })
+    }
+})//end getOrderStats
 
 module.exports = router;
