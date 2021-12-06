324a66bbf0a379276b711b9f1da30d4c133a1905
HuzaifaVizzwebsolutions <huzaifa_vizz@vizzwebsolutions.com>
2021-12-06 18:50:54 +0500

    Huzaifa COMMIT

--- api/auth.js
+++ api/auth.js
@@ -2,6 +2,7 @@ var jwt = require('express-jwt');
 var secret = 'digiebot_trading';
 
 function getTokenFromHeader(req){
+  console.log(req, 'getTokenFromHeader')
   if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token' ||
       req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
 
