4148774c041a07a1afeeb4e7f33cc569f620a9b5
HuzaifaVizzwebsolutions <huzaifa_vizz@vizzwebsolutions.com>
2021-12-14 12:47:45 +0500

    Huzaifa COMMIT

--- api/api.js
+++ api/api.js
@@ -12518,15 +12518,15 @@ async function add_user_info(user_ip, admin_id, api_key, api_secret, interface, 
       if(user_ip == '3.227.143.115'){
         ip = 'ip1.digiebot.com'
       } else if(user_ip == '3.228.180.22'){
-        ip = 'ip1.digiebot.com'
+        ip = 'ip2.digiebot.com'
       } else if(user_ip == '3.226.226.217'){
-        ip = 'ip1.digiebot.com'
+        ip = 'ip3.digiebot.com'
       } else if(user_ip == '3.228.245.92'){
-        ip = 'ip1.digiebot.com'
+        ip = 'ip4.digiebot.com'
       } else if(user_ip == '35.153.9.225'){
-        ip = 'ip1.digiebot.com'
+        ip = 'ip5.digiebot.com'
       } else if(user_ip == '54.157.102.20'){
-        ip = 'ip1.digiebot.com'
+        ip = 'ip6.digiebot.com'
       }
 
       // let url = 'http://'+ip+
